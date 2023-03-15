15798ab858e814bc13137f252982459d6fb93a51
cby3149 <cby3149@gmail.com>
2023-03-14 17:11:24 -0700

    Fix bobascope and add popup

--- packages/boba/gateway/src/services/networkService.js
+++ packages/boba/gateway/src/services/networkService.js
@@ -722,12 +722,6 @@ class NetworkService {
   }
 
   async getSevens() {
-
-    console.log("getSevens()")
-
-    // NOT SUPPORTED on LOCAL
-    if (this.networkGateway === 'local') return
-
     const response = await omgxWatcherAxiosInstance(
       this.networkConfig
     ).get('get.l2.pendingexits')
@@ -745,12 +739,6 @@ class NetworkService {
   }
 
   async getFastExits() {
-
-    console.log("getFastExits()")
-
-    // NOT SUPPORTED on LOCAL
-    if (this.networkGateway === 'local') return
-
     const response = await omgxWatcherAxiosInstance(
       this.networkConfig
     ).get('get.l2.pendingexits')
